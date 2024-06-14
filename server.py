5bb7e2a599690de5e27c3ef04e330171a0b28782
Hannes <hannes@leuchtetgruen.de>
2024-06-14 09:38:01 +0200

    if unsure, use last language

--- server.py
+++ server.py
@@ -3,12 +3,31 @@ from flask import Flask, request, send_file
 import os
 app = Flask(__name__)
 
+def remember_language(lang: str):
+    with open('last-language', 'w') as f:
+        f.writelines([lang])
+
+def last_language() -> str | None:
+    if not os.path.exists('last-language'):
+        return None
+
+    with open('last-language', 'r') as f:
+        return f.readline()
+
+
 @app.route('/v1/audio/speech', methods=['POST'])
 def speech():
     data = request.get_json()  # Get the JSON input from the POST request
     text = data['input']  # Extract the 'input' field
     prediction = cld3.get_language(text)
     lang = prediction.language
+
+    if (prediction.is_reliable):
+        remember_language(lang)
+    else:
+        last_lang = last_language()
+        if not last_language == None:
+            lang = last_lang
 
     
     with open('textfile.txt', 'w') as f:  # Write the extracted text to a file
