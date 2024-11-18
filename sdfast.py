e682a65292ee0d78121d3faf0e57f5e2890cd9e1
Hannes <hannes@leuchtetgruen.de>
2024-11-19 00:22:47 +0100

    sd fast in 8 steps

--- sdfast.py
+++ sdfast.py
@@ -5,7 +5,7 @@ import sys
 base_model_id = "runwayml/stable-diffusion-v1-5"
 repo_name = "ByteDance/Hyper-SD"
 # Take 2-steps lora as an example
-ckpt_name = "Hyper-SD15-2steps-lora.safetensors"
+ckpt_name = "Hyper-SD15-8steps-lora.safetensors"
 # Load model.
 #pipe = DiffusionPipeline.from_pretrained(base_model_id, torch_dtype=torch.float16, variant="fp16").to("cuda")
 pipe = DiffusionPipeline.from_pretrained(base_model_id, torch_dtype=torch.float32).to("cpu")
@@ -14,5 +14,5 @@ pipe.fuse_lora()
 # Ensure ddim scheduler timestep spacing set as trailing !!!
 pipe.scheduler = DDIMScheduler.from_config(pipe.scheduler.config, timestep_spacing="trailing")
 prompt= sys.argv[1]
-image=pipe(prompt=prompt, num_inference_steps=2, guidance_scale=0).images[0]
+image=pipe(prompt=prompt, num_inference_steps=8, guidance_scale=0).images[0]
 image.save('image.png')
